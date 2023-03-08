c233fb3cd29119913d5fd45fd1796f672e2c1fea
Gangura <gabrielangura@gmail.com>
2023-03-08 10:58:18 +0300

    Basics

--- Introduction to Python.py
+++ Introduction to Python.py
@@ -1,21 +1,21 @@
 # Write a program that inputs two numbers.
-num1, num2 = input("Enter two numbers: ").split()
+# num1, num2 = input("Enter two numbers: ").split()
 
 # Convert the two numbers from strings to integers
-num1 = int(num1)
-num2 = int(num2)
+# num1 = int(num1)
+# num2 = int(num2)
 
 # Add the two numbers
-Sum = num1 + num2
+# Sum = num1 + num2
 
 # Subtract the two numbers
-difference = num1 - num2
+# difference = num1 - num2
 
 # Product of the two numbers
-product = num1 * num2
+# product = num1 * num2
 
 # Quotient of the two numbers
-division = num1 / num2
+# division = num1 / num2
 
 # Print the results
 # print("{} + {} = {}".format(num1, num2, Sum))
