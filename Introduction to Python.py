c2cf726e80ffb927a4384719a3a39c8ba37a7cc1
Gangura <gabrielangura@gmail.com>
2023-03-08 10:46:00 +0300

    Initial commit

---
+++ Introduction to Python.py
@@ -0,0 +1,24 @@
+# Write a program that inputs two numbers.
+num1, num2 = input("Enter two numbers: ").split()
+
+# Convert the two numbers from strings to integers
+num1 = int(num1)
+num2 = int(num2)
+
+# Add the two numbers
+Sum = num1 + num2
+
+# Subtract the two numbers
+difference = num1 - num2
+
+# Product of the two numbers
+product = num1 * num2
+
+# Quotient of the two numbers
+division = num1 / num2
+
+# Print the results
+# print("{} + {} = {}".format(num1, num2, Sum))
+# print("{} - {} = {}".format(num1, num2, difference))
+print("{} * {} = {}".format(num1, num2, product))
+print("{} / {} = {}".format(num1, num2, division))
