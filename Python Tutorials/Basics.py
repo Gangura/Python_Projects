61ef7b2a130bc142f0cacbd9010e7a4c8951badf
Gangura <gabrielangura@gmail.com>
2023-03-09 15:09:27 +0300

    Basics

--- Python Tutorials/Basics.py
+++ Python Tutorials/Basics.py
@@ -33,14 +33,35 @@
 
 # TUTORIAL 3 EXERCISE
 # Feed the age of the person
-age = eval(input("What is your age? "))
+# age = eval(input("What is your age? "))
 
-if age < 5:
-    print("Too young for School")
-elif age == 5:
-    print("Go to Kindergarten")
-elif (age > 5) and (age <= 17):
-    grade = age - 5
-    print("Should be in grade {}".format(grade))
-else:
-    print("Go to college")
+# if age < 5:
+# print("Too young for School")
+# elif age == 5:
+# print("Go to Kindergarten")
+# elif (age > 5) and (age <= 17):
+# grade = age - 5
+# print("Should be in grade {}".format(grade))
+# else:
+# print("Go to college")
+
+# def my_function():
+# print("In my function")
+# my_function()
+
+# def my_function(counter=89):
+# print("Counter: {}".format(counter))
+
+# my_function(12)
+
+# def my_function(counter):
+# print("Counter: {}".format(counter))
+
+# my_function(12)
+
+# def my_function(counter=89):
+# print("Counter: {}".format(counter))
+
+# my_function()
+
+print("My name is Gabriel and I come from Lira", sep='....')
